be469ed8fbb972ccbd4225e8b3ad0920a2d48128
rjbogel <rudijunianto21@gmail.com>
2022-08-30 11:09:00 +0700

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:apk_ow_guci/module/home_page/home_page.dart';
 import 'package:apk_ow_guci/module/splash_screen/splash_screen_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: SplashScreenPage(),
+    home: HomePage(),
   ));
 }
--- lib/module/home_page/home_page.dart
+++ lib/module/home_page/home_page.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/material.dart';
 
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.home),
+        centerTitle: true,
+        title: const Text("Obyek Wisata Guci"),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.all(50),
+        child: Column(
+          children: [
+            FlutterLogo(
+              size: 100,
+            ),
+            Text('flutter classroom beginer'),
+            Text('student : Rudi Juniyanto'),
+            Row(
+              children: [
+                Icon(Icons.home),
+                Icon(Icons.headset),
+                Icon(Icons.settings),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
